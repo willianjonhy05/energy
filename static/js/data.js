b2375b39882859dad094a2fcbbe9db2aac385400
willianjonhy05 <willianjohny05@gmail.com>
2024-03-02 22:05:22 -0300

    Alterações em registro

---
+++ static/js/data.js
@@ -0,0 +1,17 @@
+function mascaraData(data) {
+    data = data.replace(/\D/g, '');
+    if (data.length > 2) {
+        data = data.replace(/^(\d{2})(\d)/g, '$1/$2');
+        if (data.length > 5) {
+            data = data.replace(/^(\d{2})\/(\d{2})(\d{1,4})$/, '$1/$2/$3');
+        }
+    }
+    return data;
+}
+
+
+var campoData = document.getElementById('id_data');
+
+campoData.addEventListener('input', function () {
+    this.value = mascaraData(this.value);
+});
