53ab75cc5eb1a35be03dd1d3e3aa1b58ca838edd
willianjonhy05 <willianjohny05@gmail.com>
2024-03-17 23:12:58 -0300

    Atualização na View Painel

--- energia/views.py
+++ energia/views.py
@@ -9,6 +9,8 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from reportlab.pdfgen import canvas
+from django.shortcuts import get_object_or_404
+
 
 
 # Create your views here.
@@ -86,15 +88,22 @@ class Home(TemplateView):
                 context["casas_count"] = casas_count
         return context
 
+    
 class Painel(LoginRequiredMixin, TemplateView):
     template_name = 'painel.html'
 
+    def dispatch(self, request, casa_id, *args, **kwargs):
+        casa_atual = get_object_or_404(Casa, pk=casa_id)
+        if request.user.usuario != casa_atual.proprietario:
+            return render(request, 'adm/acesso-negado.html')       
+        return super().dispatch(request, casa_id=casa_id, *args, **kwargs)
+
     def get_context_data(self, casa_id, **kwargs):
-        context = super().get_context_data(**kwargs)        
+        context = super().get_context_data(**kwargs)  
         casa = Casa.objects.get(pk=casa_id)
         registros = Registro.objects.filter(casa=casa).order_by('-data')
-        registro_recente = Registro.objects.latest('data')
-        registro_oficial_recente = Registro.objects.filter(oficial=True).latest('data')
+        registro_recente = Registro.objects.filter(casa=casa).latest('data')
+        registro_oficial_recente = Registro.objects.filter(casa=casa, oficial=True).latest('data')
         diferenca_consumo = registro_recente.consumo - registro_oficial_recente.consumo
         ultima_data = registro_recente.data
         ultima_data_oficial = registro_oficial_recente.data
@@ -105,7 +114,7 @@ class Painel(LoginRequiredMixin, TemplateView):
         prod_maxima = registro_oficial_recente.capacidade_maxima
         diferenca_ate_prod_max = prod_maxima - diferenca_injecao
         media_consumo_diario = diferenca_consumo // dias
-        total = diferenca_consumo + diferenca_injecao
+        total = diferenca_consumo + diferenca_injecao        
         context['diferenca_consumo'] = diferenca_consumo
         context['diferenca_injecao'] = diferenca_injecao
         context['diferenca_ate_prod_max'] = diferenca_ate_prod_max
@@ -119,7 +128,6 @@ class Painel(LoginRequiredMixin, TemplateView):
         context['dias'] = dias
         return context
     
-
 @login_required
 def grafico(request):
     return render(request, 'grafico.html')
