fbc5e445557024fb2aadf476e9d4df8a94b1d500
willianjonhy05 <willianjohny05@gmail.com>
2024-03-25 15:53:17 -0300

    Alteração

--- energia/views.py
+++ energia/views.py
@@ -105,7 +105,10 @@ class Painel(LoginRequiredMixin, TemplateView):
         registro_recente = Registro.objects.filter(casa=casa).latest('data')
         registro_oficial_recente = Registro.objects.filter(casa=casa, oficial=True).latest('data')
         context['registro_oficial_recente'] = registro_oficial_recente    
-        context['registro_recente'] = registro_recente    
+        context['registro_recente'] = registro_recente
+
+        if not registros.exists():
+            return render(self.request, 'registro/cadastrar.html', context=context)    
 
         if registro_oficial_recente == registro_recente:
             context["msg"] = "Informações insuficientes para gerar um Painel! Insira dados e retorne!"           
