c0fba550342da14f75e37624f8159359544da852
willianjonhy05 <willianjohny05@gmail.com>
2024-03-26 21:15:05 -0300

    Mudanças no admin

--- gestao/admin.py
+++ gestao/admin.py
@@ -3,6 +3,20 @@ from .models import Registro, Casa, Usuario
 
 # Register your models here.
 
-admin.site.register(Registro)
-admin.site.register(Casa)
-admin.site.register(Usuario)
+@admin.register(Registro)
+class RegistroAdmin(admin.ModelAdmin):
+    list_display = ["data", "casa", "consumo", "injecao", "oficial"]
+    search_fields = ["casa"]
+    list_per_page = 10
+
+@admin.register(Casa)
+class CasaAdmin(admin.ModelAdmin):
+    list_display = ["proprietario","numero", "bairro"]
+    search_fields = ["numero"]
+    list_per_page = 10
+
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ["nome","email", "telefone", "cpf"]
+    search_fields = ["cpf"]
+    list_per_page = 10
